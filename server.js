34ec227f4f2590cd8663865e9f31d46a773b8493
Nellymaina <99280364+Nellymaina@users.noreply.github.com>
2025-03-06 16:49:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const nodemailer = require('nodemailer');
 dotenv.config();
 
 const app = express();
-const PORT =process.env.PORT||5000;
+const PORT =process.env.PORT;
 app.use(express.json(), cors({
   origin: 'https://nellymaina.github.io/simplerecipes',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
